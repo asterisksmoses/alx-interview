77769e6480cd3d6e6241ff8a63eab05e0c614556
asterisksmoses <asterisksmoses@gmail.com>
2024-10-06 03:44:57 -0700

    0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+
+        for x in range(1, i):
+            row.append(triangle[i-1][x-1] + triangle[i-1][x])
+
+        row.append(1)
+
+        triangle.append(row)
+
+    return triangle
